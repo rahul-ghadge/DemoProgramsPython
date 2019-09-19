0d5cdb6de5ff4a383716f645784a7e0d8c856bee
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-09-19 23:43:30 +0530

    Update heart.py

--- heart.py
+++ heart.py
@@ -6,16 +6,16 @@ num = 6
 for row in range(num):
     for col in range(num+1):
         if (row==0 and col%3 !=0) or (row == 1 and col % 3 ==0) or ((row-col)==2) or (row+col==8):
-            print("*", end="")
+            print("* ", end="")
         else:
-            print(" ", end="")
+            print("  ", end="")
     else:
         print()
    
 # Output :   
-#   ** ** 
-#  *  *  *
-#  *     *
-#   *   * 
-#    * *  
-#     *   
+#  * *   * *   
+#*     *     * 
+#*           * 
+#  *       *   
+#    *   *     
+#      *   
