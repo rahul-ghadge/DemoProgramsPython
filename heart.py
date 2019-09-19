03ea2b976800970ad3779c91e3c03bca812faba4
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-09-19 19:02:14 +0530

    Create heart.py

---
+++ heart.py
@@ -0,0 +1,21 @@
+
+#num = int(input("Enter number > 5 to print Heart :"))
+
+num = 6
+
+for row in range(num):
+    for col in range(num+1):
+        if (row==0 and col%3 !=0) or (row == 1 and col % 3 ==0) or ((row-col)==2) or (row+col==8):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    else:
+        print()
+   
+# Output :   
+#   ** ** 
+#  *  *  *
+#  *     *
+#   *   * 
+#    * *  
+#     *   
