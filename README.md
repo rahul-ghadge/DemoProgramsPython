a8598c865c7cfbc080bd07c90c98bad1cda6f72e
rahul-ghadge <rahul.g555@gmail.com>
2020-09-02 22:34:31 +0530

    Binary Search impl

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Demo Programs in Python
+
+
+
+### https://www.toptal.com/developers/sorting-algorithms
+
+### https://visualgo.net/en
+
+### http://sorting.at/
--- search/BinaryRecursiveSearch.py
+++ search/BinaryRecursiveSearch.py
@@ -1,3 +1,5 @@
+
+
 def binaryRecursiveSearch(arr, value, low, high):
     
     if low > high:
@@ -23,8 +25,56 @@ value = 20
 found = binaryRecursiveSearch(arr, value, 0, len(arr)-1)
 print('Value found:', found)
 
+
+
+
+
+
+
+
+def binarySearchRecursive(arr, ele):
+
+    if len(arr) == 0:
+        return False
+
+    else:
+
+        mid = len(arr) // 2
+
+        if arr[mid] == ele:
+            return True
+
+        else:
+            if mid < arr[mid]:
+                return binarySearchRecursive(arr[:mid], ele)
+            else:
+                return binaryRecursiveSearch(arr[mid:], ele)
+
+
+print("\n***************************************************")
+arr = [10, 20, 30, 40, 50]
+
+value = 35
+found = binarySearchRecursive(arr, value)
+print('Value found:', found)
+
+value = 20
+found = binarySearchRecursive(arr, value)
+print('Value found:', found)
+
+
+
+
+
+
+
+
 # -------------------
 # Output
 # -------------------
+# Value found: False
+# Value found: True
+#
+# ***************************************************
 # Value found: False
 # Value found: True
